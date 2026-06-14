431c68e4cc8e95392e608b6934e540d338ce1203
ohjun <ohjun@localhost>
2026-06-14 18:45:44 -0400

    change some setting

--- src/battle/GameConfig.java
+++ src/battle/GameConfig.java
@@ -9,5 +9,5 @@ public class GameConfig
 
 	public enum GameMode { STORY, ENDLESS }
 
-	final static public int STORY_TARGET = 100;		// 剧情模式目标分数
+	final static public int STORY_TARGET = 180;		// 剧情模式目标分数
 }
